f3e162ae9312eea129a9eebe985a37d19e9578ed
hndr <yozhik20@gmail.com>
2021-05-12 22:39:24 +0900

    faqを修正

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -8,91 +8,75 @@ const Faq: React.FC = () => {
         <div className="grid grid-cols-1 gap-0 text-gray-600 md:grid-cols-2 md:gap-16">
           <div>
             <h5 className="mt-10 mb-3 font-semibold text-gray-900">
-              PODとはどういったアプリですか
+              PODとはどんなアプリですか
             </h5>
             <p>
-              Tails is a drag and drop page builder built on Top of TailwindCSS.
-              You can drop components to create a page that you can export.
+              Podcastと音楽を再生することができるアプリです。※音楽再生は現在Android版のみ対応
+              Podcast購読についてはログインIDが共通であればiOS/Androidで共有されます。
+              将来的にはタッチホイールでのシークや倍速再生、同時再生などの機能実現を目指しています。
             </p>
             <h5 className="mt-10 mb-3 font-semibold text-gray-900">
               お金はかかりますか
             </h5>
             <p>
-              Absolutely, you can try out tails for free; however, if you wish
-              to access all the components and export the page you'll need to
-              upgrade your account.
+              現在は非営利で制作しているため、無料で利用できます。
+              ユーザー数が増えた場合、無料での運営が難しくなるため
+              サブスクリプションや広告対応が入る可能性があります。
             </p>
             <h5 className="mt-10 mb-3 font-semibold text-gray-900">
               利用方法はどうなっていますか
             </h5>
             <p>
-              You can upgrade your account by visiting{" "}
-              <a
-                href="https://devdojo.com/pro"
-                className="text-indigo-500 underline"
-              >
-                The Pro Upgrade Page
-              </a>
-              . You will also gain access to many other applications and
-              sections of the site.
-              <a
-                href="https://help.hellonext.co/faq/startup-eligibility/"
-                target="_blank"
-              >
-                here
-              </a>
-              .
+              まずメールアドレスまたはgoogleアカウント（iOS版ではApple
+              IDも可能）で登録をお願いします。
+              その後、目的のポッドキャストを検索して購読する流れとなります。
+              またandroid版では本体に入っている音楽再生をすることができます。
             </p>
             <h5 className="mt-10 mb-3 font-semibold text-gray-900">
               iOS版とandroid版の違いはなんですか
             </h5>
             <p>
-              You will have unlimited access to all your pre-built pages;
-              however, if you want to be able to download and export your pages,
-              you'll need a pro account.
-              <a href="https://paddle.com" target="_blank">
-                Paddle
-              </a>{" "}
-              for processing payments.
+              Podcast購読機能は共通ですが、Android側ではローカルの音楽を取り込んで再生する機能があります。
+              開発が落ち着き次第、iOS側も対応予定です。
             </p>
           </div>
           <div>
             <h5 className="mt-10 mb-3 font-semibold text-gray-900">
-              どのようにして開発が行われていますか
+              開発はどのように行われていますか
             </h5>
             <p>
-              Implementation in your project is very simple. You can use the
-              exported page as a starting point, or you can copy and paste the
-              HTML into your own page.
+              Flutter大学のコミュニティ内でGithubを使った週1のMTGによるアジャイル開発です。
+              Google制のFlutterというクロスプラットフォーム開発フレームワークで作られています。
+              ログイン機能、ポッドキャスト購読はFirebaseを利用しています。
+              基本的にiOS/androidを1つのコードで作成されていますが、
+              音楽再生については共通で実装が行えるライブラリがないためAndroid先行で対応中です。
             </p>
             <h5 className="mt-10 mb-3 font-semibold text-gray-900">
-              PODを紹介したいのですが連絡はどうすればよいですか
+              PODを紹介したいのですが、連絡はどうすればよいですか
             </h5>
             <p>
-              You have unlimited use to the templates used in Tails; however,
-              you cannot re-use the templates to sell for others to use.
+              このページへのリンクをお貼りください。
+              その際、お問い合わせやメールでご連絡いただけると大変嬉しいです。
             </p>
             <h5 className="mt-10 mb-3 font-semibold text-gray-900">
-              PODの開発に参加したいのですがどうすればよいですか
+              PODの開発に参加したい場合はどうすればよいですか
             </h5>
             <p>
-              Of course, you can feel free to cancel your account at anytime,
-              and you can feel free to come back and upgrade again whenever
-              you're ready.
+              Flutter大学のコミュニティ内で開発されているため、Flutter大学へ入会する必要があります。
+              入会後slackに招待されますので、その中の #共同開発_music
+              channelでお声がけください！
             </p>
             <h5 className="mt-10 mb-3 font-semibold text-gray-900">
-              バグ報告や改善要望をおくりたいです
+              バグ報告や改善要望があります。
             </h5>
             <p>
-              If you need assistance implementing the templates into your
-              project you can contact support or you can visit our{" "}
               <a
-                href="https://devdojo.com/questions"
+                href="https://docs.google.com/forms/d/1T0lbgXf5TArjAgMSSyaXNijW3JZ3wvLEJEV3ADhfEBc/viewform?usp=sf_link"
                 className="text-indigo-500 underline"
               >
-                question section
+                お問い合わせ・不具合報告
               </a>
-              .
+              までお願いします。
             </p>
           </div>
         </div>

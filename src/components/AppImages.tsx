baa05c8bfb959f2c9bb514261ef5d56048136c87
hndrr <yozhik20@gmail.com>
2021-05-21 16:43:03 +0900

    不要箇所削除

--- src/components/AppImages.tsx
+++ src/components/AppImages.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import { androidSrc, iosSrc } from "../array/app_src";
-import AppImage from "./AppImage";
 import Slider from "./Slider";
 
 const AppImages: React.FC = () => {
-  const AndroidImagelist = androidSrc.map((cardImage, index) => (
-    <AppImage key={index} cardImage={cardImage} />
-  ));
-  const iOSImagelist = iosSrc.map((cardImage, index) => (
-    <AppImage key={index} cardImage={cardImage} />
-  ));
-
   return (
     <>
       <section id="android" className="py-20 -mx-4">
@@ -18,9 +10,6 @@ const AppImages: React.FC = () => {
           <h2 className="text-5xl font-bold tracking-tight text-center text-gray-900">
             Android App
           </h2>
-          {/* <div className="grid gap-8 mt-6 grid-cols-2 lg:grid-cols-4">
-            {AndroidImagelist}
-          </div> */}
           <div className="mt-6">
             <Slider imageList={androidSrc} />
           </div>
@@ -31,9 +20,6 @@ const AppImages: React.FC = () => {
           <h2 className="text-5xl font-bold tracking-tight text-center text-gray-900">
             iOS App
           </h2>
-          {/* <div className="grid gap-8 mt-6 grid-cols-2 lg:grid-cols-4">
-            {iOSImagelist}
-          </div> */}
           <div className="mt-6">
             <Slider imageList={iosSrc} />
           </div>

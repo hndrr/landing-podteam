9a85397caff2918cfe8f7343670e91448e107734
hndrr <yozhik20@gmail.com>
2021-05-21 17:51:24 +0900

    画像変更

--- src/array/app_src.ts
+++ src/array/app_src.ts
@@ -1,8 +1,10 @@
 export const iosSrc = [
+  { name: "ios signin", src: "ios600signin.png" },
   { name: "ios top", src: "ios600top.png" },
   { name: "ios list", src: "ios600list.png" },
   { name: "ios modal", src: "ios600modal.png" },
   { name: "ios detail", src: "ios600detail.png" },
+  { name: "ios setting", src: "ios600setting.png" },
 ];
 
 export const androidSrc = [
@@ -10,5 +12,7 @@ export const androidSrc = [
   { name: "android list", src: "android600list.jpg" },
   { name: "android modal", src: "android600modal.jpg" },
   { name: "android detail", src: "android600detail.jpg" },
+  { name: "android album", src: "android600album.jpg" },
+  { name: "android local", src: "android600local.jpg" },
   { name: "android setting", src: "android600setting.jpg" },
 ];

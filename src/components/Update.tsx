488a913934f571cab5a259efb52107067521a504
hndrr <yozhik20@gmail.com>
2021-08-24 01:18:48 +0900

    fix: listのレイアウトを字下げに修正

--- src/components/Update.tsx
+++ src/components/Update.tsx
@@ -17,19 +17,21 @@ const Faq: React.FC = () => {
               <span className="font-bold num">1.3.0</span>
               <span className="ml-20 num">2021/8/22</span>
               <ul className="mt-2">
-                <li>
-                  ・ログイン後、ドロワーにログイン中のメールアドレスが表示されるようになりました。
+                <li className="list-disc ml-6 mb-2">
+                  ログイン後、ドロワーにログイン中のメールアドレスが表示されるようになりました。
                 </li>
-                <li>
-                  ・登録済みポッドキャストのリストを逆順で表示できるようになりました。
+                <li className="list-disc ml-6 mb-2">
+                  登録済みポッドキャストのリストを逆順で表示できるようになりました。
                 </li>
-                <li>
-                  ・再生中のポッドキャストのエピソード、音楽アルバム曲を常に表示するステータスバーを追加しました。
+                <li className="list-disc ml-6 mb-2">
+                  再生中のポッドキャストのエピソード、音楽アルバム曲を常に表示するステータスバーを追加しました。
                 </li>
-                <li>
-                  ・再生中の音楽、アルバムリスト、ポッドキャストリストがハイライトされるようになりました。
+                <li className="list-disc ml-6 mb-2">
+                  再生中の音楽、アルバムリスト、ポッドキャストリストがハイライトされるようになりました。
                 </li>
-                <li>・ログイン、サインアップ時のバグなどを修正しました。</li>
+                <li className="list-disc ml-6 mb-2">
+                  ログイン、サインアップ時のバグなどを修正しました。
+                </li>
               </ul>
             </li>
             <li className="mb-4">
@@ -37,8 +39,8 @@ const Faq: React.FC = () => {
               <span className="font-bold num">1.2.1</span>
               <span className="ml-20 num">2021/6/24</span>
               <ul className="mt-2">
-                <li>
-                  ・Podcastの表示順が切り替えられない、レイアウトが崩れるなどの細かいバグを修正しました。
+                <li className="list-disc ml-6 mb-2">
+                  Podcastの表示順が切り替えられない、レイアウトが崩れるなどの細かいバグを修正しました。
                 </li>
               </ul>
             </li>
@@ -47,9 +49,15 @@ const Faq: React.FC = () => {
               <span className="font-bold num">1.2.0</span>
               <span className="ml-20 num">2021/5/5</span>
               <ul className="mt-2">
-                <li>・ドロワー、 問い合わせフォーム追加</li>
-                <li>・ポッドキャスト登録のUI改善</li>
-                <li>・apple/googleログインの追加</li>
+                <li className="list-disc ml-6 mb-2">
+                  ドロワー、 問い合わせフォーム追加
+                </li>
+                <li className="list-disc ml-6 mb-2">
+                  ポッドキャスト登録のUI改善
+                </li>
+                <li className="list-disc ml-6 mb-2">
+                  apple/googleログインの追加
+                </li>
               </ul>
             </li>
             <li className="mb-4">
@@ -57,15 +65,15 @@ const Faq: React.FC = () => {
               <span className="font-bold num">1.1.0</span>
               <span className="ml-20 num">2021/4/6</span>
               <ul className="mt-2">
-                <li>・いくつかのバグを修正。</li>
+                <li className="list-disc ml-6 mb-2">いくつかのバグを修正。</li>
               </ul>
             </li>
             <li className="mb-4">
               <span className="font-bold">Version </span>
               <span className="font-bold num">1.0</span>
-              <span className="ml-20 num">2021/3/27</span>
+              <span className="ml-24 num">2021/3/27</span>
               <ul className="mt-2">
-                <li>・リリース</li>
+                <li className="list-disc ml-6 mb-2">リリース</li>
               </ul>
             </li>
           </ul>
